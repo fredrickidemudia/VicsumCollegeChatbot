2d904b9c9ff3beb9a553dc10dec8d9081ca0517c
unknown <mymailserver888@gmail.com>
2025-08-20 21:57:39 +0100

    Add CSRF trusted origins

--- chatgpt_project/settings.py
+++ chatgpt_project/settings.py
@@ -115,6 +115,12 @@ USE_I18N = True
 USE_TZ = True
 
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://vicsumcollegechatbot.up.railway.app",
+]
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
